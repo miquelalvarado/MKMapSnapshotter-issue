039649a450ac1aaae1133109a7c32eeea6fbf4b0
Miquel Alvarado <miquel.alvarado@scmspain.com>
2019-07-14 19:05:35 +0200

    Create snapshot and set the result to an imageView

--- MapSnapshoterIssue/ViewController.swift
+++ MapSnapshoterIssue/ViewController.swift
@@ -7,14 +7,37 @@
 //
 
 import UIKit
+import MapKit
 
 class ViewController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
+    @IBOutlet weak var snapshotImageView: UIImageView!
+
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        makeSnapshot()
     }
 
+    private func makeSnapshot() {
+        let options = MKMapSnapshotter.Options()
+        let center = CLLocationCoordinate2D(latitude: 37.334722, longitude: -122.008889)
+        let span = MKCoordinateSpan(latitudeDelta: 0.06, longitudeDelta: 0.06)
+        options.region = MKCoordinateRegion(center: center, span: span)
+        options.size = snapshotImageView.frame.size
+        let snapshotter = MKMapSnapshotter(options: options)
+        snapshotter.start(with: .global(qos: .userInitiated)) { (snapshot, _) in
+            guard let snapshot = snapshot else { return }
+            let image = snapshot.image
+            UIGraphicsBeginImageContextWithOptions(image.size, true, image.scale)
+            image.draw(at: .zero)
 
+            let mapImage = UIGraphicsGetImageFromCurrentImageContext()
+            UIGraphicsEndImageContext()
+
+            DispatchQueue.main.async {
+                self.snapshotImageView.image = mapImage
+            }
+        }
+    }
 }
 
