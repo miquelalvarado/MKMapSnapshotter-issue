537ebdf3665900f30acd337ee152e8db7dd34f17
Miquel Alvarado <miquel.alvarado@scmspain.com>
2019-07-14 18:30:32 +0200

    Initial Commit

---
+++ MapSnapshoterIssue/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MapSnapshoterIssue
+//
+//  Created by Miquel Alvarado on 14/07/2019.
+//  Copyright © 2019 Miquel Alvarado. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
